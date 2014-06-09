0cfe7a58b5546ade54c6f2eba7d13d75ec57c590
Nick Burne <nickburne@users.noreply.github.com>
2014-06-09 22:55:55 +0100

    Update pootlepress-text-widget.php

--- pootlepress-text-widget.php
+++ pootlepress-text-widget.php
@@ -1,9 +1,9 @@
 <?php
 /*
-Plugin Name: Pootle Text Widget
+Plugin Name: PootlePress Visual Editor Widget
 Plugin URI: http://pootlepress.com/
 Description: A WordPress widget that has a visual and HTML editor and WooThemes & WooCommerce shortcode buttons
-Version: 1.0.0
+Version: 0.9
 Author: PootlePress
 Author URI: http://pootlepress.com/
 License: GPL version 2 or later - http://www.gnu.org/licenses/old-licenses/gpl-2.0.html
@@ -17,6 +17,6 @@ require_once( 'classes/class-pootlepress-text-widget-plugin.php' );
 require_once( 'classes/class-pootlepress-text-widget.php' );
 
 $GLOBALS['pootlepress_text_widget'] = new Pootlepress_Text_Widget_Plugin( __FILE__ );
-$GLOBALS['pootlepress_text_widget']->version = '1.0.0';
+$GLOBALS['pootlepress_text_widget']->version = '0.9';
 
 ?>
